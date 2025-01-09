4a9a5b828f544f69094d8a2026f0e0956bf6f94e
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2025-01-08 19:50:57 -0800

    started landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Landing from "./components/Landing";
 
 export default function Home() {
-  return <div className="h-[5000px]"></div>;
+  return (
+    <div className="h-[5000px] flex flex-col py-24">
+      <Landing />
+    </div>
+  );
 }
